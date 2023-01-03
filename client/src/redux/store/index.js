21a1d16387df9fd98d0a4272b8880bc7c912d563
LNMIG <migliorenicolas@yahoo.com.ar>
2023-01-03 11:37:00 -0300

    FIX: store creation updated

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,12 +1,10 @@
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from '@redux-devtools/extension'
+import { configureStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-
 import reducer from '../reducer'
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+const store = configureStore(
+  { reducer },
+  applyMiddleware(thunk)
 )
 
 export default store
